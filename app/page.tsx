fde9fa5106953d9485f8a22c2cf04792a29eb041
VisshGit <sdmrevenuecourts@gmail.com>
2026-09-03 15:45:35 +0530

    Clean up Home component by removing unused sections
    Removed unused section imports and components from Home.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,6 @@ import WelcomeGate from "@/components/sections/WelcomeGate";
 import Hero from "@/components/sections/Hero";
 import ScratchReveal from "@/components/sections/ScratchReveal";
 import Countdown from "@/components/sections/Countdown";
-import Story from "@/components/sections/Story";
-import Gallery from "@/components/sections/Gallery";
-import Events from "@/components/sections/Events";
-import Venue from "@/components/sections/Venue";
-import RSVP from "@/components/sections/RSVP";
-import Closing from "@/components/sections/Closing";
 
 export default function Home() {
   return (
@@ -18,12 +12,6 @@ export default function Home() {
       <Hero />
       <ScratchReveal />
       <Countdown />
-      <Story />
-      <Gallery />
-      <Events />
-      <Venue />
-      <RSVP />
-      <Closing />
     </WelcomeGate>
   );
 }
