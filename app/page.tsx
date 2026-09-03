85045fef2f487ad1c78c63c1184595bf216e1241
VisshGit <sdmrevenuecourts@gmail.com>
2026-09-03 15:50:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,12 @@ import WelcomeGate from "@/components/sections/WelcomeGate";
 import Hero from "@/components/sections/Hero";
 import ScratchReveal from "@/components/sections/ScratchReveal";
 import Countdown from "@/components/sections/Countdown";
+import Story from "@/components/sections/Story";
+import Gallery from "@/components/sections/Gallery";
+import Events from "@/components/sections/Events";
+import Venue from "@/components/sections/Venue";
+import RSVP from "@/components/sections/RSVP";
+import Closing from "@/components/sections/Closing";
 
 export default function Home() {
   return (
@@ -12,6 +18,12 @@ export default function Home() {
       <Hero />
       <ScratchReveal />
       <Countdown />
+      <Story />
+      <Gallery />
+      <Events />
+      <Venue />
+      <RSVP />
+      <Closing />
     </WelcomeGate>
   );
 }
